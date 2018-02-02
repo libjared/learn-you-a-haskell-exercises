e815ff52e636532a76ab8ff3e3f0c3864f46a768
libjared <libjared@users.noreply.github.com>
2018-02-02 13:18:43 -0500

    Update 02-starting-out.hs

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -53,4 +53,4 @@ insertElem x k l = concat [take k l, [x], drop k l]
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate n l = concat [drop n l, take n l]
